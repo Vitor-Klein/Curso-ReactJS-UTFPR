4da92e2d150308c92107b1b104329182dc64b8ae
Vitor-Klein <vitorhklein2@gmail.com>
2024-12-10 21:24:46 -0300

    finish

---
+++ cat-facts/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import api from "./services/api";
+
+function App() {
+  const [fact, setFact] = useState({});
+
+  async function handleFact() {
+    const response = await api.get("/fact");
+    setFact(response.data);
+  }
+
+  useEffect(() => {
+    handleFact();
+  }, []);
+
+  return (
+    <div className="backgroundContaienr">
+      <div className="container">
+        <div className="catHead">
+          <div className="leftCatEar"></div>
+          <div className="rightCatEar"></div>
+        </div>
+        <button onClick={() => handleFact()} className="factButton">
+          Novo fato!
+        </button>
+
+        <span className="factText">{fact.fact}</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
